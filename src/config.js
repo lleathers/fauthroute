ee7b4f3989725dcf1f1027c0a2a8d976f2574208
Larry Leathers <lleathers@naehe.com>
2025-01-08 07:57:01 -0500

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyC1OtTAblIfFQaafmrjFRnNdw62cZwj4ic",
+  authDomain: "localhost:5003",
+  projectId: "vuedowne",
+  storageBucket: "vuedowne.firebasestorage.app",
+  messagingSenderId: "890672733251",
+  appId: "1:890672733251:web:037e50bbcbe7136329c8f6",
+  measurementId: "G-XG8H0NT9MT" 
+};
