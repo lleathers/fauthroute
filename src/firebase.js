1dc3682b826c1a60a27219685e3394091bf66f2b
Larry Leathers <lleathers@naehe.com>
2025-01-17 12:15:53 -0500

    Adapt firestore emulation for specific use, to prototype.

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,15 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import config from './config';
+
+// import config from './config';
+
 
 function initialize(existingApp) {
-  const firebaseApp = existingApp || initializeApp(config);
-//  const firebaseApp = initializeApp(default);
-//  const firebaseApp = existingApp;
+
+//  const firebaseApp = existingApp || initializeApp(config);
+
+  const firebaseApp = existingApp || initializeApp();
   const auth = getAuth(firebaseApp);
   const firestore = getFirestore(firebaseApp);
   const storage = getStorage(firebaseApp);
